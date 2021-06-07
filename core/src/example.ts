ba9aa7d64c10f496c92e1bfc1197e7a9ad90f91b
Vincent Gromakowski <gromav@amazon.com>
2021-06-07 14:19:42 +0200

    feat: adding contributing guidelines example

--- core/src/example.ts
+++ core/src/example.ts
@@ -1,33 +1,43 @@
 import * as cdk from '@aws-cdk/core';
 
 /**
- * Properties for Example Construct
- *
+ * @summary The properties for the Example Construct class.
  */
 
 export interface ExampleProps {
+  /**
+   * Name used to qualify the CfnOutput in the Stack
+   * @default -  Set to 'defaultMessage' if not provided
+   */
   readonly name?: string;
+  /**
+   * Value used in the CfnOutput in the Stack
+   * @default -  Set to 'defaultValue!' if not provided
+   */
   readonly value?: string;
 }
 
 /**
- * Example Construct to help onboarding contributors
- *
+ * @Summary Example Construct to help onboarding contributors.
  */
 
 export class Example extends cdk.Construct {
 
   /**
-   * Return only a CfnOutput
-   * @param scope the Scope of the CDK Stack
-   * @param id the ID of the CDK Stack
+   * Constructs a new instance of the Example class with default CfnOutput.
+   * CfnOutput can be customized.
+   * @param {cdk.App} scope the Scope of the CDK Stack
+   * @param {string} id the ID of the CDK Stack
+   * @param {ExampleProps} props the ExampleProps [properties]{@link ExampleProps}
+   * @since 1.0.0
+   * @access public
    */
 
   constructor(scope: cdk.Construct, id: string, props: ExampleProps) {
     super(scope, id);
 
     // add a fake CFN Output to the Stack
-    // we use an export name
+    // use an export name because the output name is defined by CDK
     new cdk.CfnOutput(this, 'message', {
       exportName: props.name ? props.name: 'defaultMessage',
       value: props.value ? props.value: 'defaultValue!',
--- core/test/example.test.ts
+++ core/test/example.test.ts
@@ -6,12 +6,12 @@ test('example construct', () => {
 
   const exampleStack = new cdk.Stack();
 
-  // Instantiate Example Construct with customer Props
+  // Instantiate Example Construct with custom Props
   new Example(exampleStack, 'CustomExample', { name: 'message', value: 'hello!' });
   // Instantiate Example Construct without Props for getting default parameters
   new Example(exampleStack, 'DefaultExample', {});
 
-  // Test if CfnOutput is similar to customer Props provided
+  // Test if CfnOutput is similar to custom Props provided
   expect(exampleStack).toHaveOutput({ exportName: 'message', outputValue: 'hello!' });
   // Test if CfnOutput is similar to default parameters
   expect(exampleStack).toHaveOutput({ exportName: 'defaultMessage', outputValue: 'defaultValue!' });
