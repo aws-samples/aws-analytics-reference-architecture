5c328d4857aa6a0220ef05e17eb188f01b14fea6
Vincent Gromakowski <gromav@amazon.com>
2021-07-09 19:52:27 +0200

    refactoring emr on eks

--- core/src/emr-eks-nodegroup.ts
+++ core/src/emr-eks-nodegroup.ts
@@ -1,3 +1,5 @@
+// add default nodegroups as in here https://github.com/aws-samples/aws-analytics-reference-architecture/blob/feature/data-generator/core/src/data-generator.ts
+
 export interface EmrEksNodegroupProps{
 
 }
