defb7f5716dc24d6ba9932fcbb8a680d70377f10
yadavzjy <77168801+yadavzjy@users.noreply.github.com>
2021-09-18 22:07:11 +0100

    corrected de-serializer

--- core/src/data-exporter-stack.ts
+++ core/src/data-exporter-stack.ts
@@ -7,26 +7,31 @@ import * as logs from '@aws-cdk/aws-logs';
 
 
 export interface KfstreamProps {
-  bucket: Bucket
+  bucket: Bucket,
 }
+
+
 
 export class Kfstream extends cdk.Construct {
   public readonly ingestionStream: firehose.CfnDeliveryStream;
-  
+
   constructor(scope: cdk.Construct, id: string, props: KfstreamProps) {
     super(scope, id);
 
     const role = new iam.Role(this, 'FirehoseRole', {
       assumedBy: new iam.ServicePrincipal('firehose.amazonaws.com'),
     });
-    
+
+    const gluePolicy = iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSGlueServiceRole");
+      role.addManagedPolicy(gluePolicy);
+
     // Create log group for storing forehose logs.
     const logGroup = new logs.LogGroup(this, 'data-exporter-log-group', {
       logGroupName: '/data-exporter/firehose/',
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       retention: logs.RetentionDays.FIVE_DAYS
     });
-    
+
     // Create the Kinesis Firehose log stream.
     const firehoseLogStream = new logs.LogStream(this, 'kinesis-firehose-log', {
       logGroup: logGroup,
@@ -50,18 +55,22 @@ export class Kfstream extends cdk.Construct {
           logGroupName: logGroup.logGroupName,
           logStreamName: firehoseLogStream.logStreamName
         },
-      roleArn: role.roleArn,
+        roleArn: role.roleArn,
         errorOutputPrefix: 'failed-data/',
         prefix: 'ingest-data/',
-        compressionFormat: 'snappy',
+        compressionFormat: 'UNCOMPRESSED',
         s3BackupMode: 'Disabled',
         dataFormatConversionConfiguration: {
           enabled: true,
           inputFormatConfiguration: {
-            deserializer: openXJsonSerDe
+            deserializer: {
+              openXJsonSerDe: {}
+           }
          },
           outputFormatConfiguration: {
-            serializer: orcSerDe
+            serializer: {
+              orcSerDe: {}
+           }
          },
           schemaConfiguration: {
             roleArn: role.roleArn,
@@ -79,3 +88,4 @@ export class Kfstream extends cdk.Construct {
     }
   }
 }
+
