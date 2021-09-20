5390857841f3c8b56e01e280ee403f6d0fe047d3
yadavzjy <77168801+yadavzjy@users.noreply.github.com>
2021-09-20 15:13:27 +0100

    Add test with user inputs

--- core/test/data-exporter.test.ts
+++ core/test/data-exporter.test.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import cdk = require('@aws-cdk/core');
 import s3 = require('@aws-cdk/aws-s3');
 
-import { Kfstream } from '../src/data-exporter-stack';
+import { DataExporter } from './data-exporter-stack';
 
 export class MainStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -17,9 +17,13 @@ export class MainStack extends cdk.Stack {
       encryption: s3.BucketEncryption.S3_MANAGED
     });
 
-    // Creation of the firehose stream 
-    const kfstream = new Kfstream(this, 'Streamng layer', {
-      bucket: rawBucket
+    // Creation of the firehose stream
+    const Dataexporter = new DataExporter(this, 'Streamng layer', {
+      bucket: rawBucket,
+      sinkArn: 'test',
+      database: 'test2',
+      table: 'table'
+
     })
 }
 }
