9d2eadc941467738f6421561e5d5d54d150324f8
ijemmy <ijemmy@users.noreply.github.com>
2021-10-13 16:34:16 +0200

    debug

--- core/.projenrc.js
+++ core/.projenrc.js
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 const { basename, join, dirname, relative } = require('path');
-const path = require('path/posix');
 const glob = require('glob');
 
 
