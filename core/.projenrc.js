2670ae37e67337639c18a64224d329e5ef400aa5
ijemmy <ijemmy@users.noreply.github.com>
2021-10-14 10:07:32 +0200

    debug

--- core/.projenrc.js
+++ core/.projenrc.js
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-const { basename, join, dirname, relative } = require('path');
+const { basename, join, dirname, relative, sep } = require('path');
 const glob = require('glob');
 
 
@@ -99,25 +99,40 @@ for (const from of glob.sync('src/**/resources')) {
  * Task to pip install all Python Lambda functions inlib
  */
 
-const pipInstallTask = project.addTask('pip-install', {
+
+const pipInstallTask = project.addTask('bundle', {
   description: 'pip install all folders in lib that has requirements.txt',
 });
 
 for (const dirPath of findAllPythonLambdaDir('src')) {
-  const dirPathInLib = dirPath.replace('src', 'lib');
-  const target = dirname(dirPathInLib);
-  const pipCmd = `pip3 install -r ${dirPathInLib} --target ${target}`;
+  // const dirPathInLib = dirPath.replace('src', 'lib');
+  // const target = dirname(dirPathInLib);
+  // const pipCmd = `pip3 install -r ${dirPathInLib} --target ${target}`;
 
-  pipInstallTask.exec(pipCmd);
+  // pipInstallTask.exec(pipCmd);
+
+  // dirPath can be in the form of 
+  // 1. /src/<construct-name>/resources/lambdas 
+  // 2. /src/<construct-name>/resources/lambdas/<lambda-name>
+  const dirName = basename(dirPath);
+  const constructName = dirPath.split(sep)[1];
+  const uniqueName = `${constructName}-${dirName}`;
+  const taskName = `bundle:${uniqueName}`;
+
+  console.log(`Generating task "${taskName}"...`);
+  const lambdaBundleTask = addPythonLambdaFunctionBundleTask(taskName, uniqueName, dirPath);
+
+  console.log(`Add task "${taskName}" as a part of "bundle" target`);
+  pipInstallTask.spawn(lambdaBundleTask);
 }
 
+project.gitignore.addPatterns(`build`);
+project.npmignore.addPatterns(`build`);
 /**
  * Run `copy-resources` and `pip-install` as part of compile
  */
 project.compileTask.exec('npx projen copy-resources');
 project.compileTask.exec('npx projen pip-install');
-// project.testCompileTask.exec('npx projen copy-resources');
-// project.testCompileTask.exec('npx projen pip-install');
 
 /**
  * Find all directory that has a Python package.
@@ -129,8 +144,61 @@ project.compileTask.exec('npx projen pip-install');
 function findAllPythonLambdaDir(rootDir) {
 
   return glob.sync(`${rootDir}/**/requirements.txt`).map((pathWithReq) => {
-    return pathWithReq;
+    return dirname(pathWithReq);
   });
+}
+
+
+/**
+ * Add a projen task for bundling a Python Lambda.
+ *
+ * The function use Docker to pip install dependencies into `./package`.
+ * Docker is required for some libraries that needs compilation during intallation.
+ * If we compile it on our OS (Mac or Windows), it won't work on Lambda.
+ *
+ * We use a Docker from SAM as it's more stable (changed less often)
+ * and simpler to use than CDK's build image.
+ *
+ * After that, we zip the whole directory next to this directory.
+ *
+ * @param taskName Name of the task to be added
+ * @param cwd Current workign directory
+ * @param dirPath Directory name of the Python Lambda function
+ */
+ function addPythonLambdaFunctionBundleTask(taskName, uniqueName, dirPath) {
+   // cwd is the /core folder
+  console.log('process.cwd()', process.cwd());
+  console.log('taskName', taskName);
+  console.log('uniqueName', uniqueName);
+  console.log('dirPath', dirPath);
+  
+
+  const bundleTask = project.addTask(taskName, {
+    description: `Install dependency and package Python file into for Lambda function:${dirPath}`,
+  });
+  
+  const volumeBindFolder = join('build', 'docker-vol');
+  bundleTask.exec(`rm -rf ${volumeBindFolder}`)
+  bundleTask.exec(`mkdir -p ${volumeBindFolder}`);
+
+
+  // Copy all files ('<dirpath>/.') to 'volumeBindFolder'
+  bundleTask.exec(`cp -r ${dirPath + '/.'} ${volumeBindFolder}`);
+
+  // Pip install and compiles dependencies via Docker SAM image
+  bundleTask.exec([
+    'docker run',
+    `-v $PWD/${volumeBindFolder}:/var/task`, // Mapping from host full path to Docker container //TODO: only local
+    '"public.ecr.aws/sam/build-python3.8"', //Image name
+    // '/bin/sh -c "pip install -r requirements.txt -t ./; exit"', //Commands to run in the image
+    '/bin/sh -c "ls -la; exit"', //Commands to run in the image
+  ].join(' '));
+
+  // todo: copy result out
+
+  // Zip file to next to the lambda directory
+
+  return bundleTask;
 }
 
 project.synth();
