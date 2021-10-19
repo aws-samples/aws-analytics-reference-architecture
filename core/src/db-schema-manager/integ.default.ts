e6558aebfcc24274c4f9d011056d43c4e7561004
Chazal <chazalf@88665a14a6c3.ant.amazon.com>
2021-10-19 16:10:42 +0200

    put back integ test file

--- core/src/db-schema-manager/integ.default.ts
+++ core/src/db-schema-manager/integ.default.ts
@@ -1,12 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-// import * as path from 'path';
+import * as path from 'path';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as redshift from '@aws-cdk/aws-redshift';
 import * as cdk from '@aws-cdk/core';
 
-// import { FlywayRunner } from '.';
+import { FlywayRunner } from '.';
 
 const integTestApp = new cdk.App();
 const stack = new cdk.Stack(integTestApp, 'fywayRunnerTest');
@@ -14,7 +14,7 @@ const stack = new cdk.Stack(integTestApp, 'fywayRunnerTest');
 const vpc = new ec2.Vpc(stack, 'Vpc');
 
 const dbName = 'testdb';
-new redshift.Cluster(stack, 'Redshift', {
+const cluster = new redshift.Cluster(stack, 'Redshift', {
   removalPolicy: cdk.RemovalPolicy.DESTROY,
   masterUser: {
     masterUsername: 'admin',
@@ -23,9 +23,9 @@ new redshift.Cluster(stack, 'Redshift', {
   defaultDatabaseName: dbName,
 });
 
-// new FlywayRunner(stack, 'testMigration', {
-//   migrationScriptsFolderAbsolutePath: path.join(__dirname, './resources/sql'),
-//   cluster: cluster,
-//   vpc: vpc,
-//   databaseName: dbName,
-// });
+new FlywayRunner(stack, 'testMigration', {
+  migrationScriptsFolderAbsolutePath: path.join(__dirname, './resources/sql'),
+  cluster: cluster,
+  vpc: vpc,
+  databaseName: dbName,
+});
