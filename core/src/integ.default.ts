bffab1bc9de6034e6949bbb696d83939bcafc305
Lotfi Mouhib <mouhib@amazon.com>
2021-11-11 15:07:56 +0000

    updated the integ.default.ts

--- core/src/integ.default.ts
+++ core/src/integ.default.ts
@@ -2,21 +2,28 @@
 // SPDX-License-Identifier: MIT-0
 
 import { App, Stack } from '@aws-cdk/core';
-import { EmrEksCluster } from '.';
+import {DataPlatform, StudioAuthMode, StudioUserDefinition} from '.';
+
+const envInteg = { account: '214783019211', region: 'eu-west-1' };
 
 const mockApp = new App();
-const stack = new Stack(mockApp, 'stack');
-const cluster = new EmrEksCluster(stack, 'testCluster', { eksAdminRoleArn: 'arn:aws:iam::XXXXXXXXXXXX:role/YYYYYYYYYY' });
+const stack = new Stack(mockApp, 'deployment', { env: envInteg });
 
-
-cluster.addEmrVirtualCluster({
-  name: 'sometest',
-  eksNamespace: 'sometest',
-  createNamespace: true,
+const dept1 = DataPlatform.getOrCreate(stack, 'my-platform', {
+  eksAdminRoleArn: 'arn:aws:iam::123456789012:role/Admin',
 });
 
-cluster.addEmrVirtualCluster({
-  name: 'anothertest',
-  eksNamespace: 'anothertest',
-  createNamespace: true,
+dept1.addNotebookPlatform('unit1', {
+  studioName: 'mystudio1',
+  emrVCNamespace: 'mystudio1ns',
+  studioAuthMode: StudioAuthMode.SSO,
+  acmCertificateArn: 'arn:aws:acm:eu-west-1:123456789012:certificate/8a5dceb1-ee9d-46a5-91d2-7b4a1ea0b64d',
 });
+
+let userList: StudioUserDefinition[] = [{
+  identityName: '', /*<username or Group as it appears in SSO>*/
+  identityType: 'USER' || 'SSO',
+  executionPolicyNames: ['policyManagedEndpoint'], // The name of the policy to be used by the role for an EMR on EKS managedendpoint
+}];
+
+dept1.addUsersNotebookPlatform('unit1', userList);
--- core/src/notebooks-data-platform/dataplatform.ts
+++ core/src/notebooks-data-platform/dataplatform.ts
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: MIT-0
 
 import { KubernetesVersion } from '@aws-cdk/aws-eks';
-import { CfnOutput, Construct } from '@aws-cdk/core';
+import { CfnOutput, Construct, Stack } from '@aws-cdk/core';
 import { EmrEksCluster } from '../emr-eks-cluster';
+//import { EmrEksNodegroup } from '../emr-eks-nodegroup';
 import { DataPlatformNotebook, DataPlatformNotebookProp, StudioUserDefinition } from './dataplatform-notebook';
 
 
@@ -24,9 +25,24 @@ export interface DataPlatformProps {
 
 export class DataPlatform extends Construct {
 
+  public static getOrCreate(scope: Construct, stackName: string, props: DataPlatformProps) {
+
+    const stack = Stack.of(scope);
+    const id = `${stackName}`;
+
+    let dataPlatform: DataPlatform;
+
+    if (stack.node.tryFindChild(id) == undefined) {
+      dataPlatform = new DataPlatform(stack, id, props);
+    }
+
+    return stack.node.tryFindChild(id) as DataPlatform || dataPlatform!;
+  }
+
   private readonly emrEks: EmrEksCluster;
   private readonly dataPlatformMapping: Map<string, DataPlatformNotebook>;
   private list: string[] = [];
+
 
   constructor(scope: Construct, id: string, props: DataPlatformProps) {
     super(scope, id);
