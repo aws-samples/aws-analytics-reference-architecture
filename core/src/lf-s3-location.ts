300e2abfd34e1f5ef7b75fdaa3519b8edf84968e
Vincent Gromakowski <vgkowski@users.noreply.github.com>
2022-03-28 22:14:50 +0200

    fix: add KMS key permissions to the IAM role used for Lake Formation data access (#299)

--- core/src/lf-s3-location.ts
+++ core/src/lf-s3-location.ts
@@ -1,109 +1,98 @@
 import * as lakeformation from '@aws-cdk/aws-lakeformation';
 import { Construct} from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
+import { Bucket } from '@aws-cdk/aws-s3';
 import { Location } from '@aws-cdk/aws-s3';
 import { PolicyStatement, Role, ServicePrincipal, } from '@aws-cdk/aws-iam';
 
 /**
- * The props for LF-S3-Location Construct.
- */
+* The props for LF-S3-Location Construct.
+*/
 export interface LakeFormationS3LocationProps {
-   /**
-    * S3 bucket to be registered with Lakeformation
-    */
-    s3bucket:Location;
+  /**
+  * S3 location to be registered with Lakeformation
+  */
+  s3Location: Location;
 }
 
-  /**
-   * This CDK construct aims to register an S3 Location for Lakeformation with Read and Write access.
-   * 
-   * This construct is based on an IAM role with 2 policies folowing the least privilege AWS best practices:
-   * * Policy 1 is for GetObject, PutObject, DeleteObject from S3 bucket
-   * * Policy 2 is to list S3 Buckets
-   * 
-   * Policy 1 takes as an input S3 object arn
-   * Policy 2 takes as an input S3 bucket arn
-   * 
-   * 
-   * The CDK construct instantiate the cfnresource in order to register the S3 location with Lakeformation using the IAM role defined above.
-   * 
-   * Usage example:
-   * ```typescript
-   * import * as cdk from '@aws-cdk/core';
-   * import { LakeformationS3Location } from 'aws-analytics-reference-architecture';
-   * 
-   * const exampleApp = new cdk.App();
-   * const stack = new cdk.Stack(exampleApp, 'LakeformationS3LocationStack');
-   * 
-   * new LakeformationS3Location(stack, 'MyLakeformationS3Location', {
-   * s3bucket:{
-      bucketName: 'test',
-      objectKey: 'test',
-    }
-   * });
-   * ```
-   */
+/**
+* This CDK construct aims to register an S3 Location for Lakeformation with Read and Write access.
+* 
+* This construct instantiate 2 objects:
+* * An IAM role with read/write permissions to the S3 location and read access to the KMS key used to encypt the bucket 
+* * A CfnResource is based on an IAM role with 2 policies folowing the least privilege AWS best practices:
+* * Policy 1 is for GetObject, PutObject, DeleteObject from S3 bucket
+* * Policy 2 is to list S3 Buckets
+* 
+* Policy 1 takes as an input S3 object arn
+* Policy 2 takes as an input S3 bucket arn
+* 
+* 
+* The CDK construct instantiate the cfnresource in order to register the S3 location with Lakeformation using the IAM role defined above.
+* 
+* Usage example:
+* ```typescript
+* import * as cdk from '@aws-cdk/core';
+* import { LakeformationS3Location } from 'aws-analytics-reference-architecture';
+* 
+* const exampleApp = new cdk.App();
+* const stack = new cdk.Stack(exampleApp, 'LakeformationS3LocationStack');
+* 
+* new LakeformationS3Location(stack, 'MyLakeformationS3Location', {
+*   s3bucket:{
+*     bucketName: 'test',
+*     objectKey: 'test',
+*   }
+* });
+* ```
+*/
 export class LakeformationS3Location extends Construct {
-   
-    constructor(scope: Construct, id: string, props: LakeFormationS3LocationProps) {
-      super(scope, id);
-
-        /**
-         * Create an Amazon IAM Role used by Lakeformation to register S3 location
-         */
-        const role = new Role(this, 'LFS3AccessRole', {
-               assumedBy: new ServicePrincipal('lakeformation.amazonaws.com'),
-        });
-         
-        // add policy to access S3 for Read and Write
-        role.addToPolicy(
-            new PolicyStatement({
-                resources: [
-                s3.Bucket.fromBucketName(
-                    this,
-                    "BucketByName",
-                    props.s3bucket.bucketName
-                    ).arnForObjects(props.s3bucket.objectKey)
-                ],
-                actions: [
-                    's3:GetObject',
-                    's3:PutObject',
-                    's3:DeleteObject',
-                ],
-            }),
-        );
-
-        // add policy to list S3 bucket
-        role.addToPolicy(
-            new PolicyStatement({
-                resources: [
-                s3.Bucket.fromBucketName(
-                    this,
-                    "BucketName",
-                    props.s3bucket.bucketName
-                    ).bucketArn
-                ],
-                actions: [
-                    's3:ListBucket'
-                ],
-            }),
-        );
-
-             
-
-      //// The code below shows an example of how to instantiate the cfnresource
-        new lakeformation.CfnResource(this, 'MyCfnResource', {
-            resourceArn: s3.Bucket.fromBucketName(
-            this,
-            "BucketByNameCfn",
-            props.s3bucket.bucketName
-            ).arnForObjects(props.s3bucket.objectKey),
-
-            useServiceLinkedRole: false,
-      
-            // the properties below are optional
-            roleArn: role.roleArn
-        });  
-
+  
+  constructor(scope: Construct, id: string, props: LakeFormationS3LocationProps) {
+    super(scope, id);
+  
+    // Create a bucket from the S3 location
+    const bucket = Bucket.fromBucketName(this, "LocationBucket", props.s3Location.bucketName);
+    
+    // Create an Amazon IAM Role used by Lakeformation to register S3 location
+    const role = new Role(this, 'LFS3AccessRole', {
+      assumedBy: new ServicePrincipal('lakeformation.amazonaws.com'),
+    });
+    
+    // add policy to access S3 for Read and Write
+    role.addToPolicy(
+      new PolicyStatement({
+        resources: [
+          bucket.arnForObjects(props.s3Location.objectKey),
+          bucket.bucketArn,
+        ],
+        actions: [
+          's3:GetObject',
+          's3:PutObject',
+          's3:DeleteObject',
+          's3:ListBucket',
+        ],
+      }),
+    );
+        
+    // add policy to access KMS key used for the bucket encryption 
+    if (bucket.encryptionKey){
+      role.addToPolicy(
+        new PolicyStatement({
+          resources: [
+            bucket.encryptionKey?.keyArn,
+          ],
+          actions: [
+            'kms:Decrypt',
+            'kms:GenerateDataKey',
+          ],
+        }),
+      );
     }
+
+    new lakeformation.CfnResource(this, 'MyCfnResource', {
+      resourceArn: bucket.arnForObjects(props.s3Location.objectKey),
+      useServiceLinkedRole: false,
+      roleArn: role.roleArn,
+    });
+  }
 }
--- core/test/unit/lf-s3-location.test.ts
+++ core/test/unit/lf-s3-location.test.ts
@@ -1,37 +1,52 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
+
 /**
  * Tests LakeformationS3Location
  *
- * @group unit/feature/lf-s3-location
+ * @group unit/lakeformation/s3location
  */
 
-// import * as cdk from '@aws-cdk/core';
- // import * from '@types/jest';
- //import { SdkProvider } from 'aws-cdk/lib/api/aws-auth';
- //import { CloudFormationDeployments } from 'aws-cdk/lib/api/cloudformation-deployments';
- import { LakeformationS3Location } from '../../src/lf-s3-location';
- import '@aws-cdk/assert/jest';
- import { Stack } from '@aws-cdk/core';
+import { LakeformationS3Location } from '../../src/lf-s3-location';
+import '@aws-cdk/assert/jest';
+import { Stack } from '@aws-cdk/core';
+import { Match, Template } from '@aws-cdk/assertions';
 
-  test('LakeformationS3Location construct', () => {
+describe('S3Location test', () => {
 
   const lfS3Stack = new Stack();  
  
-  new LakeformationS3Location(lfS3Stack, 'CustomExample', 
-   { 
-     s3bucket: {
+  new LakeformationS3Location(lfS3Stack, 'CustomExample', { 
+     s3Location: {
        bucketName: 'test',
        objectKey: 'test',
      }
-   }
-   );
+  });
   
+  const template = Template.fromStack(lfS3Stack);
 
-  // Check if the Stack has a Role
- //expect(lfS3Stack).toContain('LFS3AccessRole');
-  expect(lfS3Stack).toHaveResource('AWS::IAM::Role');
+  test('S3Location should create the proper Lake Formation CfnResource', () => {
+    template.hasResourceProperties('AWS::LakeFormation::Resource',
+      Match.objectLike({
+        UseServiceLinkedRole: false,
+        RoleArn: Match.anyValue(),
+        ResourceArn: Match.objectLike({
+          "Fn::Join": [
+            "",
+            [
+              "arn:",
+              {
+                "Ref": "AWS::Partition"
+              },
+              ":s3:::test/test"
+            ]
+          ]
+        }),
+      })
+    );
+  });
 
-  // TODO: check the role has AmazonSSMManagedInstanceCore managed policy
+  test('S3Location should create the proper IAM role', () => {
+    template.resourceCountIs('AWS::IAM::Role', 1);
+  });
 });
- 
