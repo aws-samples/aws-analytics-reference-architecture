9f9137862c8f79956a4d3ae473608590dd5d1124
Florian Chazal <chazalf@amazon.com>
2022-06-05 09:03:01 +0200

    chore: fix release workflow

--- core/.projenrc.js
+++ core/.projenrc.js
@@ -136,6 +136,10 @@ project.packageTask.spawn(project.tasks.tryFind('package-all'));
 
 const package = project.tasks.tryFind('package-all');
 
+package.spawn('delete-jar', {
+  exec: 'rm src/db-schema-manager/resources/flyway-lambda/flyway-all.jar',
+});
+
 project.addTask('test:destroy', {
   exec: 'cdk destroy --app=./lib/integ.default.js',
 });
