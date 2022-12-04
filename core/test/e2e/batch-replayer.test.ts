9f72aede6da9d3d742e8512fd48184136c796fe9
Daisy Riley <rileydai@amazon.co.uk>
2022-12-04 18:17:36 +0000

    test: Add tests for additional database targets for BatchReplayer

--- core/test/e2e/batch-replayer.test.ts
+++ core/test/e2e/batch-replayer.test.ts
@@ -8,34 +8,121 @@
 */
 
 import { Bucket } from 'aws-cdk-lib/aws-s3';
-import * as cdk from 'aws-cdk-lib';
-import { RemovalPolicy } from 'aws-cdk-lib';
-import { deployStack , destroyStack } from './utils';
+import { App, Stack, aws_ec2, aws_rds, aws_dynamodb, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
+import { Cluster } from '@aws-cdk/aws-redshift-alpha';
+import { deployStack, destroyStack } from './utils';
 
 import { BatchReplayer } from '../../src/data-generator/batch-replayer';
 import { PreparedDataset } from '../../src/data-generator/prepared-dataset';
 
-jest.setTimeout(600000);
+jest.setTimeout(3000000);
 // GIVEN
-const integTestApp = new cdk.App();
-const stack = new cdk.Stack(integTestApp, 'BatchReplayerE2eTest');
+const integTestApp = new App();
+const stack = new Stack(integTestApp, 'BatchReplayerE2eTest');
+
+const defaultName = 'test';
 
 const sinkBucket = new Bucket(stack, 'SinkBucket', {
   removalPolicy: RemovalPolicy.DESTROY,
   autoDeleteObjects: true,
 });
 
+const vpc = new aws_ec2.Vpc(stack, 'Vpc');
+
+const securityGroup = new aws_ec2.SecurityGroup(stack, 'SecurityGroup', {vpc});
+
+const ddbTable = new aws_dynamodb.Table(stack, 'DynamoDB', {
+  partitionKey: { name: defaultName, type: aws_dynamodb.AttributeType.STRING },
+  removalPolicy: RemovalPolicy.DESTROY,
+});
+
+const redshift = new Cluster(stack, 'Redshift', {
+  masterUser: { masterUsername: 'admin' },
+  defaultDatabaseName: defaultName,
+  removalPolicy: RemovalPolicy.DESTROY,
+  vpc,
+});
+const redshiftCreds = redshift.secret ? redshift.secret.secretArn : '';
+
+const auroraMySQL = new aws_rds.DatabaseCluster(stack, 'AuroraMySQL', {
+  engine: aws_rds.DatabaseClusterEngine.auroraMysql({ version: aws_rds.AuroraMysqlEngineVersion.VER_2_08_1 }),
+  defaultDatabaseName: defaultName,
+  removalPolicy: RemovalPolicy.DESTROY,
+  instanceProps: { vpc },
+});
+const auroraMysqlCreds = auroraMySQL.secret ? auroraMySQL.secret.secretArn : '';
+
+const auroraPostgres = new aws_rds.DatabaseCluster(stack, 'AuroraPostgres', {
+  engine: aws_rds.DatabaseClusterEngine.auroraPostgres({ version: aws_rds.AuroraPostgresEngineVersion.VER_12_11 }),
+  defaultDatabaseName: defaultName,
+  removalPolicy: RemovalPolicy.DESTROY,
+  instanceProps: { vpc },
+});
+const auroraPostgresCreds = auroraPostgres.secret ? auroraPostgres.secret.secretArn : '';
+
+const rdsMySQL = new aws_rds.DatabaseInstance(stack, 'MySQL', {
+  engine: aws_rds.DatabaseInstanceEngine.mysql({ version: aws_rds.MysqlEngineVersion.VER_8_0_28 }),
+  databaseName: defaultName,
+  removalPolicy: RemovalPolicy.DESTROY,
+  vpc,
+});
+const rdsMysqlCreds = rdsMySQL.secret ? rdsMySQL.secret.secretArn : '';
+
+const rdsPostgres = new aws_rds.DatabaseInstance(stack, 'PostgreSQL', {
+  engine: aws_rds.DatabaseInstanceEngine.postgres({ version: aws_rds.PostgresEngineVersion.VER_14_2 }),
+  databaseName: defaultName,
+  removalPolicy: RemovalPolicy.DESTROY,
+  vpc,
+});
+const rdsPostgresCreds = rdsPostgres.secret ? rdsPostgres.secret.secretArn : '';
+
 const batchReplayer = new BatchReplayer(stack, 'BatchReplay', {
   dataset: PreparedDataset.RETAIL_1_GB_STORE_SALE,
   sinkBucket: sinkBucket,
+  ddbTable: ddbTable,
+  redshiftTableName: defaultName,
+  redshiftConnection: redshiftCreds,
+  redshiftSchema: defaultName,
+  auroraMysqlTableName: defaultName,
+  auroraMysqlConnection: auroraMysqlCreds,
+  auroraMysqlSchema: defaultName,
+  auroraPostgresTableName: defaultName,
+  auroraPostgresConnection: auroraPostgresCreds,
+  auroraPostgresSchema: defaultName,
+  mysqlTableName: defaultName,
+  mysqlConnection: rdsMysqlCreds,
+  mysqlSchema: defaultName,
+  postgresTableName: defaultName,
+  postgresConnection: rdsPostgresCreds,
+  postgresSchema: defaultName,
+  databaseVpc: vpc,
+  databaseVpcSG: securityGroup,
 });
 
 new BatchReplayer(stack, 'BatchReplay2', {
   dataset: PreparedDataset.RETAIL_1_GB_CUSTOMER,
   sinkBucket: sinkBucket,
+  ddbTable: ddbTable,
+  redshiftTableName: defaultName,
+  redshiftConnection: redshiftCreds,
+  redshiftSchema: defaultName,
+  auroraMysqlTableName: defaultName,
+  auroraMysqlConnection: auroraMysqlCreds,
+  auroraMysqlSchema: defaultName,
+  auroraPostgresTableName: defaultName,
+  auroraPostgresConnection: auroraPostgresCreds,
+  auroraPostgresSchema: defaultName,
+  mysqlTableName: defaultName,
+  mysqlConnection: rdsMysqlCreds,
+  mysqlSchema: defaultName,
+  postgresTableName: defaultName,
+  postgresConnection: rdsPostgresCreds,
+  postgresSchema: defaultName,
+  databaseVpc: vpc,
+  databaseVpcSG: securityGroup,
 });
 
-new cdk.CfnOutput(stack, 'DatasetName', {
+new CfnOutput(stack, 'DatasetName', {
   value: batchReplayer.dataset.tableName,
   exportName: 'DatasetName',
 });
--- core/test/unit/data-generator/batch-replayer.test.ts
+++ core/test/unit/data-generator/batch-replayer.test.ts
@@ -7,25 +7,101 @@
  * @group unit/data-generator/batch-replayer
  */
 
-import { Duration, Stack } from "aws-cdk-lib";
+import { Cluster } from '@aws-cdk/aws-redshift-alpha';
+import {
+  aws_dynamodb,
+  aws_ec2,
+  aws_rds, Duration,
+  RemovalPolicy,
+  Stack,
+} from 'aws-cdk-lib';
 
-import { BatchReplayer, PreparedDataset } from "../../../src/data-generator";
-import { Bucket } from "aws-cdk-lib/aws-s3";
-import { Template } from "aws-cdk-lib/assertions";
+import { Template } from 'aws-cdk-lib/assertions';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BatchReplayer, PreparedDataset } from '../../../src';
+import { IVpc } from 'aws-cdk-lib/aws-ec2';
+import {ITable} from "aws-cdk-lib/aws-dynamodb";
 
 let testStack: Stack;
 let bucket: Bucket;
 let batchReplayer: BatchReplayer;
 let template: Template;
+let ddbTable: ITable;
+let defaultName = 'test';
+let vpc: IVpc;
 
 beforeEach(() => {
   testStack = new Stack();
+
   bucket = new Bucket(testStack, 'Bucket');
-  batchReplayer = new BatchReplayer(testStack, "TestBatchReplayer", {
+
+  vpc = new aws_ec2.Vpc(testStack, 'Vpc');
+
+  const securityGroup = new aws_ec2.SecurityGroup(testStack, 'SecurityGroup', {vpc});
+
+  ddbTable = new aws_dynamodb.Table(testStack, 'DynamoDB', {
+    partitionKey: { name: defaultName, type: aws_dynamodb.AttributeType.STRING },
+    removalPolicy: RemovalPolicy.DESTROY,
+  });
+
+  new Cluster(testStack, 'Redshift', {
+    masterUser: { masterUsername: defaultName },
+    defaultDatabaseName: defaultName,
+    removalPolicy: RemovalPolicy.DESTROY,
+    vpc,
+  });
+
+  new aws_rds.DatabaseCluster(testStack, 'AuroraMySQL', {
+    engine: aws_rds.DatabaseClusterEngine.auroraMysql({ version: aws_rds.AuroraMysqlEngineVersion.VER_2_08_1 }),
+    defaultDatabaseName: defaultName,
+    removalPolicy: RemovalPolicy.DESTROY,
+    instanceProps: { vpc },
+  });
+
+  new aws_rds.DatabaseCluster(testStack, 'AuroraPostgres', {
+    engine: aws_rds.DatabaseClusterEngine.auroraPostgres({ version: aws_rds.AuroraPostgresEngineVersion.VER_12_11 }),
+    defaultDatabaseName: defaultName,
+    removalPolicy: RemovalPolicy.DESTROY,
+    instanceProps: { vpc },
+  });
+
+  new aws_rds.DatabaseInstance(testStack, 'MySQL', {
+    engine: aws_rds.DatabaseInstanceEngine.mysql({ version: aws_rds.MysqlEngineVersion.VER_8_0_28 }),
+    databaseName: defaultName,
+    removalPolicy: RemovalPolicy.DESTROY,
+    vpc,
+  });
+
+  new aws_rds.DatabaseInstance(testStack, 'PostgreSQL', {
+    engine: aws_rds.DatabaseInstanceEngine.postgres({ version: aws_rds.PostgresEngineVersion.VER_14_2 }),
+    databaseName: defaultName,
+    removalPolicy: RemovalPolicy.DESTROY,
+    vpc,
+  });
+
+  batchReplayer = new BatchReplayer(testStack, 'TestBatchReplayer', {
     dataset: PreparedDataset.RETAIL_1_GB_WEB_SALE,
     frequency: Duration.seconds(120),
     sinkBucket: bucket,
-    sinkObjectKey: 'test'
+    sinkObjectKey: defaultName,
+    ddbTable: ddbTable,
+    redshiftTableName: defaultName,
+    redshiftConnection: defaultName,
+    redshiftSchema: defaultName,
+    auroraMysqlTableName: defaultName,
+    auroraMysqlConnection: defaultName,
+    auroraMysqlSchema: defaultName,
+    auroraPostgresTableName: defaultName,
+    auroraPostgresConnection: defaultName,
+    auroraPostgresSchema: defaultName,
+    mysqlTableName: defaultName,
+    mysqlConnection: defaultName,
+    mysqlSchema: defaultName,
+    postgresTableName: defaultName,
+    postgresConnection: defaultName,
+    postgresSchema: defaultName,
+    databaseVpc: vpc,
+    databaseVpcSG: securityGroup,
   });
   template = Template.fromStack(testStack);
 });
@@ -37,8 +113,6 @@ test("BatchReplayer should use given frequency", () => {
 test("BatchReplayer should use default frequency", () => {
   const batchReplayerWithNoFreqProp = new BatchReplayer(testStack, "TestBatchReplayerWithNoFreqProp", {
     dataset: PreparedDataset.RETAIL_1_GB_WEB_SALE,
-    sinkBucket: bucket,
-    sinkObjectKey: 'test',
   });
   expect(batchReplayerWithNoFreqProp.frequency).toBe(60);
 });
@@ -46,8 +120,6 @@ test("BatchReplayer should use default frequency", () => {
 test("BatchReplayer should use given max output file size", () => {
   const batchReplayerWithFilesizeProp = new BatchReplayer(testStack, "TestBatchReplayerWithNoFreqProp", {
     dataset: PreparedDataset.RETAIL_1_GB_WEB_SALE,
-    sinkBucket: bucket,
-    sinkObjectKey: 'test',
     outputFileMaxSizeInBytes: 20480,
   });
   expect(batchReplayerWithFilesizeProp.outputFileMaxSizeInBytes).toBe(20480);
@@ -56,14 +128,20 @@ test("BatchReplayer should use given max output file size", () => {
 test("BatchReplayer should use default max output file size 100MB", () => {
   const batchReplayerWithNoFilesizeProp = new BatchReplayer(testStack, "TestBatchReplayerWithNoFreqProp", {
     dataset: PreparedDataset.RETAIL_1_GB_WEB_SALE,
-    sinkBucket: bucket,
-    sinkObjectKey: 'test',
   });
   expect(batchReplayerWithNoFilesizeProp.outputFileMaxSizeInBytes).toBe(100 * 1024 * 1024);
 });
 
-test("BatchReplayer should create 2 lambda functions from Dockerfile with 15 mins timeout", () => {
-  template.hasResourceProperties("AWS::Lambda::Function", {
+test('sinkObjectKey should only be defined if sinkBucket is defined', () => {
+  const batchReplayerWithNoFilesizeProp = new BatchReplayer(testStack, 'TestBatchReplayerWithNoSinkBucket', {
+    dataset: PreparedDataset.RETAIL_1_GB_WEB_SALE,
+  });
+  expect(batchReplayerWithNoFilesizeProp.sinkObjectKey).toBe(undefined);
+  expect(batchReplayer.sinkObjectKey).toBe(`${defaultName}/${batchReplayer.dataset.tableName}`);
+});
+
+test('BatchReplayer should create 2 lambda functions from Dockerfile with 15 mins timeout', () => {
+  template.hasResourceProperties('AWS::Lambda::Function', {
     //"PackageType": "Image",
     "Timeout": 900
   })
