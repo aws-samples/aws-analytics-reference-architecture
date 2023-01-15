084c4b31ed8f54d10174a186309ca160ed66887a
mouhib <mouhib@amazon.com>
2023-01-15 16:36:56 +0000

    add more logic to dockerbuild

--- core/src/docker-builder/docker-build.ts
+++ core/src/docker-builder/docker-build.ts
@@ -1,34 +1,59 @@
-import { BuildSpec, PipelineProject } from "aws-cdk-lib/aws-codebuild";
+import { BuildSpec, LinuxBuildImage, Project } from "aws-cdk-lib/aws-codebuild";
 import { Repository } from "aws-cdk-lib/aws-ecr";
 import { Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { stringList } from "aws-sdk/clients/datapipeline";
 import { Construct } from "constructs";
 
 export interface DockerBuilderProps {
-
-    readonly repositoryArn: string;
-
-    readonly repositoryName: string;
+  readonly repositoryName: string;
 }
 
 export class DockerBuilder extends Construct {
+
+    private readonly ecrUI : string;
+
     constructor (scope: Construct, id: string, props: DockerBuilderProps) {
 
-        super (scope, id);
+        super (scope, id); 
 
-        let codeBuildRole = new Role(this, '', {
+        let codeBuildRole = new Role(this, 'codebuildarn', {
             assumedBy: new ServicePrincipal('codebuild.amazonaws.com'),
         });
 
-        const ecrRepo: Repository = new Repository(this, '', {
+        const ecrRepo: Repository = new Repository(this, `ecr-${props.repositoryName}`, {
             repositoryName: props.repositoryName
         });
+        
+        this.ecrUI = ecrRepo.repositoryUri;
 
-        const buildPipeline: PipelineProject = new PipelineProject(this, '', {
-            buildSpec: BuildSpec.fromSourceFilename('./buildspec-docker-build.yaml'),
+        let commands = [
+            'echo logging into docker',
+            '$(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)',
+            'echo Build start',
+            'docker logout'
+            ];
+
+        new Project(this, 'DockerImageDeployProject', {
+            buildSpec: BuildSpec.fromObject({
+              version: '0.2',
+              phases: {
+                build: {
+                  commands: commands,
+                },
+              },
+            }),
             environment: {
-                privileged: true,
+              privileged: true,
+              buildImage: LinuxBuildImage.STANDARD_5_0,
             },
             role: codeBuildRole,
-        });
+          });
+
+        console.log(ecrRepo.repositoryName);
     }
+
+    public publishImage (dockerfilePath: string) {
+
+    }
+  
 }
---
+++ core/src/docker-builder/index.ts
@@ -0,0 +1,1 @@
+export { DockerBuilder, DockerBuilderProps } from './docker-build';
