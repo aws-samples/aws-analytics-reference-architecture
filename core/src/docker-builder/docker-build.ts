779cf9459973c346e4414bfb775f647841450f06
mouhib <mouhib@amazon.com>
2023-01-15 20:29:15 +0000

    improve management of Dockerfile handling in s3

--- core/src/docker-builder/docker-build.ts
+++ core/src/docker-builder/docker-build.ts
@@ -1,4 +1,4 @@
-import { Aws, CfnOutput, CustomResource, RemovalPolicy } from "aws-cdk-lib";
+import { Aws, CfnOutput, CustomResource, RemovalPolicy, Stack } from "aws-cdk-lib";
 import { BuildSpec, LinuxBuildImage, Project } from "aws-cdk-lib/aws-codebuild";
 import { Repository } from "aws-cdk-lib/aws-ecr";
 import { Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
@@ -18,12 +18,13 @@ export class DockerBuilder extends Construct {
   private readonly dockerBuildPublishCrToken: string;
   private readonly assetBucket: AraBucket;
   private readonly codebuildProjectName: string;
+  private readonly ecrName: string;
 
   constructor(scope: Construct, id: string, props: DockerBuilderProps) {
 
     super(scope, id);
 
-    this.assetBucket = AraBucket.getOrCreate(this, { bucketName: 'docker-builder-assets', encryption: BucketEncryption.KMS_MANAGED });
+    this.assetBucket = AraBucket.getOrCreate(this, { bucketName: `${Stack.of(this).stackName}-ara-docker-assets`, encryption: BucketEncryption.KMS_MANAGED });
 
     let codeBuildRole = new Role(this, 'codebuildarn', {
       assumedBy: new ServicePrincipal('codebuild.amazonaws.com'),
@@ -36,6 +37,7 @@ export class DockerBuilder extends Construct {
     });
 
     this.ecrURI = ecrRepo.repositoryUri;
+    this.ecrName = props.repositoryName;
 
     let commands = [
       'echo logging into docker',
@@ -87,7 +89,7 @@ export class DockerBuilder extends Construct {
     const containerImage =  new CustomResource(this, 'testCR', {
       serviceToken: this.dockerBuildPublishCrToken,
       properties: {
-        s3Path: `s3://${this.assetBucket.bucketName}/myDockerFile/Dockerfile`,
+        s3Path: `s3://${this.assetBucket.bucketName}/${this.ecrName}/${tag}/Dockerfile`,
         tag: tag,
         ecrURI: this.ecrURI,
         codebuildProjectName: this.codebuildProjectName,
